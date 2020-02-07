3bd112d3ca376ca76f23a27bbe5662a57715c031
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-02-07 15:38:55 -0500

    Create eventListener.js

---
+++ DianeAlexander/eventListener.js
@@ -0,0 +1,8 @@
+// Homework to change two elements with JavaScript
+
+document.addEventListener("click", mouseEvent); 
+
+function mouseEvent (theMainText)  {
+document.getElementById("button1").style.backgroundColor="#7bfc03";
+document.getElementById("element2").innerHTML="Say WHAT???";
+}
